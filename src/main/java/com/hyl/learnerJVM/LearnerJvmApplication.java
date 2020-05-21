1d921fa83ac5ca3f5a5c2dd70689acb670127ed7
huangyongliang <363212682@qq.com>
2020-05-22 01:38:28 +0800

    init

---
+++ src/main/java/com/hyl/learnerJVM/LearnerJvmApplication.java
@@ -0,0 +1,13 @@
+package com.hyl.learnerJVM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerJvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnerJvmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyl/learnerJVM/LearnerJvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyl.learnerJVM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnerJvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
