03b9153690117834090ad183a3cb1f7cfc61dc90
huangyl <363212682@qq.com>
2020-09-06 22:30:08 +0800

    添加openjdk，javac源码

---
+++ src/main/java/com/hyl/learnerJVM/jvmstack/test/StringTest.java
@@ -0,0 +1,12 @@
+package com.hyl.learnerJVM.jvmstack.test;
+
+/**
+ * @author hyl
+ * @version v1.0: StringTest.java, v 0.1 2020/9/6 1:02 $
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = new String("b");
+    }
+}
